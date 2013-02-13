9198271648322ac6a9588247d8480831541d4919
Raphael Randschau <nicolai86@me.com>
2013-02-13 09:57:08 +0100

    update Railtie

--- lib/automated_commands/railtie.rb
+++ lib/automated_commands/railtie.rb
@@ -2,7 +2,7 @@
 module AutomatedCommands
   class Railtie < ::Rails::Railtie
     initializer "automated_commands.environment" do |app|
-      if ::Rails.env.test?
+      if ::Rails.env.test? && defined?(::IRB)
         ::IRB::ExtendCommandBundle.send :include, ::AutomatedCommands::Rails::ConsoleMethods
       end
     end
